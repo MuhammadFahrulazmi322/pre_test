4e6af5ee60cf2cbc7cc4b70d6ed3e8c7f47bff52
MuhammadFahrulAzmi322 <fazmi322@gmail.com>
2024-06-26 23:53:39 +0700

    update data image and make blog, luxury, feauture

--- scripts.js
+++ scripts.js
@@ -1,25 +1,54 @@
 $(document).ready(function () {
-    let currentIndex = 0;
-    const images = $('.carousel img');
-    const dots = $('.dot');
-  
-    function showImage(index) {
-      const offset = -index * 150; // Sesuaikan ukuran gambar
-      $('.carousel').css('transform', `translateX(${offset}px)`);
-      dots.removeClass('bg-gray-900').addClass('bg-gray-400');
-      dots.eq(index).removeClass('bg-gray-400').addClass('bg-gray-900');
-    }
-  
-    dots.click(function () {
-      currentIndex = $(this).index();
-      showImage(currentIndex);
-    });
-  
+  let currentIndex = 0;
+  const images = $(".carousel img");
+  const dots = $(".dot");
+
+  function showImage(index) {
+    const offset = -index * 150; // Sesuaikan ukuran gambar
+    $(".carousel").css("transform", `translateX(${offset}px)`);
+    dots.removeClass("bg-gray-900").addClass("bg-gray-400");
+    dots.eq(index).removeClass("bg-gray-400").addClass("bg-gray-900");
+  }
+
+  dots.click(function () {
+    currentIndex = $(this).index();
     showImage(currentIndex);
-  
-    // Hamburger menu toggle
-    $('#menu-btn').click(function () {
-      $('#mobile-menu').toggleClass('hidden');
-    });
   });
-  
+
+  showImage(currentIndex);
+
+  // Hamburger menu toggle
+  $("#menu-btn").click(function () {
+    $("#mobile-menu").toggleClass("hidden");
+  });
+});
+
+const blogContainer = document.getElementById("blog-container");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+blogContainer.addEventListener("mousedown", (e) => {
+  isDown = true;
+  blogContainer.classList.add("active");
+  startX = e.pageX - blogContainer.offsetLeft;
+  scrollLeft = blogContainer.scrollLeft;
+});
+
+blogContainer.addEventListener("mouseleave", () => {
+  isDown = false;
+  blogContainer.classList.remove("active");
+});
+
+blogContainer.addEventListener("mouseup", () => {
+  isDown = false;
+  blogContainer.classList.remove("active");
+});
+
+blogContainer.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - blogContainer.offsetLeft;
+  const walk = (x - startX) * 2; // scroll-fast
+  blogContainer.scrollLeft = scrollLeft - walk;
+});
