858bc291f35f206e139324cce590d31dfa73316d
MuhammadFahrulazmi322 <fazmi322@gmail.com>
2024-06-27 16:51:51 +0700

    update styling, mobile in collection and footer is not finish

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   const dots = $(".dot");
 
   function showImage(index) {
-    const offset = -index * 150; // Sesuaikan ukuran gambar
+    const offset = -index * 300; // Sesuaikan ukuran gambar
     $(".carousel").css("transform", `translateX(${offset}px)`);
     dots.removeClass("bg-gray-900").addClass("bg-gray-400");
     dots.eq(index).removeClass("bg-gray-400").addClass("bg-gray-900");
